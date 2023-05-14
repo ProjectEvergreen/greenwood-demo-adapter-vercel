c08f1c50c2821f70f973c8e3c0e90e45122d8b9a
Owen Buckley <owenbuckley13@gmail.com>
2023-05-13 20:31:57 -0400

    update import name

--- api/fragment.js
+++ api/fragment.js
@@ -1,12 +1,12 @@
 // TODO should use src or public?  depends on dev vs production mode?
-import { handler as greeting } from '../public/api/fragment.js';
+import { handler as fragment } from '../public/api/fragment.js';
 
 export default async function handler (request, response) {
   const { url, headers } = request;
   const req = new Request(new URL(url, `http://${headers.host}`), {
     headers: new Headers(headers)
   });
-  const res = await greeting(req);
+  const res = await fragment(req);
 
   // TODO need to handle all Response properties like headers
   // https://vercel.com/docs/concepts/functions/serverless-functions/runtimes/node-js#node.js-request-and-response-objects
